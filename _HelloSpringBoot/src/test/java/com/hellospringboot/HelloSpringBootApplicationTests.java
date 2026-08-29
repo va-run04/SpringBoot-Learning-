19a7ea11e917c17044952cc50660340df9f9ee0f
Vijayagopal <vijayagopal.sb@gmail.com>
2026-08-30 00:22:02 +0530

    Update on pom.xml and test package refactored

---
+++ _HelloSpringBoot/src/test/java/com/hellospringboot/HelloSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
